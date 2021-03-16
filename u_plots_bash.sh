6cb50ea55cf2cc9602c7c9f3674fed6539a5e5a1
Marshall <msborrus@gmail.com>
2021-03-16 14:43:27 -0400

    updated matlab slurm script to include rms

--- u_plots_bash.sh
+++ u_plots_bash.sh
@@ -2,11 +2,16 @@
 #SBATCH --job-name=matlab_u_plotter
 #SBATCH --output=matlab_u_plotter."%j".out
 #SBATCH --error=matlab_u_plotter."%j".err
-#SBATCH --partition=normal
+#SBATCH --partition=serc
 #SBATCH --time=00:30:00
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=8G
 #SBATCH --mail-type=ALL
 
 module load matlab
+module load system rclone
+
 matlab -nodisplay < u_plots.m
+matlab -nodisplay < RMS_plots.m
+
+rclone copy /home/users/mborrus/Matlab_HPC/plots remote:plot_folder
