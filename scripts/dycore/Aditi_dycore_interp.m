acff1007b4b28aaf294cc979de93514850736590
Marshall <msborrus@gmail.com>
2021-04-26 16:02:37 -0400

    gdf

--- scripts/dycore/Aditi_dycore_interp.m
+++ scripts/dycore/Aditi_dycore_interp.m
@@ -21,8 +21,11 @@ P_inter1=[0.0212    0.0330    0.0746    0.1505    0.2785    0.4820    0.8030    
  10.0500   13.2500   17.2000   22.1000   28.0000   35.1000   43.6500   53.7500   65.6000   79.5000   95.7000  114.4000  135.8500 ...
  160.4500  188.5500  220.4000  256.4000  297.0000  342.5500  393.5500  450.5000  513.8000  584.0000  661.7000  747.5000  841.9500 925.0000];
 P_inter=P_inter1';
-​%%atmos_file_name = dir(fullfile(pwd, 'atmos_daily_*')).name;
+
+​atmos_file_name = dir(fullfile(pwd, 'atmos_daily_*')).name;
+
 atmos_file_name = strcat('atmos_daily_',num2str(str2num(folderpath(70:71))/10-1),'.nc')
+
 print('u comp name has been written')
 
 u=ncread(​atmos_file_name,'ucomp');
